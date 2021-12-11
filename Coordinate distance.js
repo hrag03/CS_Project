5207e5faa7673d09ec37af2ef95e515c371f5cb0
Hrag Sousani <50873462+hrag03@users.noreply.github.com>
2021-12-11 10:33:51 +0400

    Delete Coordinate distance.js

--- Coordinate distance.js
+++
@@ -1,9 +0,0 @@
-// Calculating coordinate distances.
-
-let x1 = 1;
-let x2 = 4;
-let y1 = 1;
-let y2 = 4;
-const distanceFormula = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5;
-
-console.log(distanceFormula);
